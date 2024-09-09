2de19e6831a2e9e0d9899849075387dc38e790a3
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-09-09 17:10:05 +0200

    förstasidan filler

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const FirstPage = () => {
-    return (
-        <div>FirstPage</div>
+    return (<>
+        <div> <small>FirstPage</small></div>
+        <br />
+        <div className='text-center'>
+            <p>
+                Förstasidan är inte byggd ännu 😅
+            </p>
+
+
+
+
+            <Link to="/feed">
+                <div className='btn btn-outline-primary m-2'>
+                    Till nyhetsflödet istället!
+                </div>
+            </Link>
+
+        </div>
+
+
+
+
+
+
+    </>
+
     )
 }
 
